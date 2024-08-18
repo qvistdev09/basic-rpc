3bbb7b2c5c5f312c44676af42d3e76809d82a40c
Oscar Lindqvist <qvistdev09@gmail.com>
2024-08-18 09:34:34 -0700

    update client to set procedure name in url

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,16 +1,16 @@
 import { RpcServer } from "../server/rpc-server.js";
 import { InferredClient } from "../types.js";
 
-export function createClient<T extends RpcServer<any>>(rpcEndpoint: string) {
+export function createClient<T extends RpcServer<any>>(rpcEndpoint: string = "/api/procedures/") {
   async function callRpc(
     procedure: string,
     payload: any,
     headers?: Record<string, string>,
     abortController?: AbortController
   ) {
-    const response = await fetch(rpcEndpoint, {
+    const response = await fetch(`${rpcEndpoint}${procedure}`, {
       method: "POST",
-      body: JSON.stringify({ payload: payload ?? null, procedure }),
+      body: JSON.stringify(payload ?? null),
       headers: { ...(headers ?? {}), ["Content-Type"]: "application/json" },
       signal: abortController?.signal,
     });
